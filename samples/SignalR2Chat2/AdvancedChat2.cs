683be82e89b7a669570dc4023601418e573e5d37
Liangying Wei <lianwei@microsoft.com>
2018-06-29 13:34:37 +0800

    Add a sample to repro the bug when hubname contains .

---
+++ samples/SignalR2Chat2/AdvancedChat2.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalR2Chat
+{
+    [HubName("advancedChat.a")]
+    public class AdvancedChat2 : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).echo(name, message + " (echo from server)");
+        }
+
+        public void JoinGroup(string name, string groupName)
+        {
+            Groups.Add(Context.ConnectionId, groupName).Wait();
+            Clients.Group(groupName).echo("_SYSTEM_", $"{name} joined {groupName} with connectionId {Context.ConnectionId}");
+        }
+
+        public void LeaveGroup(string name, string groupName)
+        {
+            Groups.Remove(Context.ConnectionId, groupName).Wait();
+            Clients.Client(Context.ConnectionId).echo("_SYSTEM_", $"{name} leaved {groupName}");
+            Clients.Group(groupName).echo("_SYSTEM_", $"{name} leaved {groupName}");
+        }
+
+        public void SendGroup(string name, string groupName, string message)
+        {
+            Clients.Group(groupName).echo(name, message);
+        }
+
+        public void SendGroups(string name, IList<string> groups, string message)
+        {
+            Clients.Groups(groups).echo(name, message);
+        }
+
+        public void SendGroupExcept(string name, string groupName, string[] connectionIdExcept, string message)
+        {
+            Clients.Groups(new List<string> { groupName }, connectionIdExcept).echo(name, message);
+        }
+
+        public void SendUser(string name, string userId, string message)
+        {
+            Clients.User(userId).echo(name, message);
+        }
+
+        public void SendUsers(string name, IList<string> userIds, string message)
+        {
+            Clients.Users(userIds).echo(name, message);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ samples/SignalR2Chat2/Chat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR2Chat2
+{
+    [HubName("Chat.A")]
+    public class Chat : Hub
+    {
+        public void Hello(string name, string message)
+        {
+            int count = 10;
+            int i = 0;
+            Enumerable.Repeat<Func<int>>(
+                    () =>
+                    {
+                        Clients.All.hello(name, $"round {i}: {message}");
+                        i++;
+                        return i;
+                    }
+                , count).Select(s => s()).ToList();
+        }
+    }
+}
